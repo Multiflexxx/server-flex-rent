5b0dc9b445cc9a435049f0dead44618b65b3f492
NerdyStuff <NerdyStuff@users.noreply.github.com>
2020-09-03 16:18:21 +0200

    Added Database connector

--- server/src/util/database/connector.ts
+++ server/src/util/database/connector.ts
@@ -1,16 +1,32 @@
-export class Connector {
-  mariadb = require('mariadb');
-  credentials: any = '../../../database.json';
-  pool: any = this.mariadb.createPool(
+ import { InternalServerErrorException } from '@nestjs/common';
+ 
+ export class Connector {
+  private static mariadb = require('mariadb');
+  private static credentials: any = '../../../database.json';
+  private static pool: any = Connector.mariadb.createPool(
     {
-      host: this.credentials.host,
-      port: this.credentials.port,
-      user: this.credentials.user,
-      password: this.credentials.password,
-      database: this.credentials.database,
-      connectionLimit: this.credentials.connectionLimit
+      host: Connector.credentials.host,
+      port: Connector.credentials.port,
+      user: Connector.credentials.user,
+      password: Connector.credentials.password,
+      database: Connector.credentials.database,
+      connectionLimit: Connector.credentials.connectionLimit
     });
 
     // TODO: Create functions
+    
+    public static async executeQuery(): Promise<any> {
+      let result = null;
+      try {
+        result = await Connector.pool.getConnection().query("Select * From Test");
+      } catch(err) {
+        throw new InternalServerErrorException("Something went wrong");
+      } finally {
+        // Close connection
+        result.release();
+      }
+      
+
+    }
 
 }
