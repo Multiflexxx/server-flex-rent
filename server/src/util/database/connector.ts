877fcb67c5c7c5d3d35f735a1209d0961f6934e6
NerdyStuff <NerdyStuff@users.noreply.github.com>
2020-09-03 15:20:27 +0200

    Added connector

--- server/src/util/database/connector.ts
+++ server/src/util/database/connector.ts
@@ -1,1 +1,16 @@
-// TODO
+export class Connector {
+  mariadb = require('mariadb');
+  credentials: any = '../../../database.json';
+  pool: any = this.mariadb.createPool(
+    {
+      host: this.credentials.host,
+      port: this.credentials.port,
+      user: this.credentials.user,
+      password: this.credentials.password,
+      database: this.credentials.database,
+      connectionLimit: this.credentials.connectionLimit
+    });
+
+    // TODO: Create functions
+
+}
