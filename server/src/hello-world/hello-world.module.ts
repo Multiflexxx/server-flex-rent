4f09977447fe0286253197003c14138c750e0138
Grimmig18 <benno.grimm@gmx.de>
2020-09-03 16:17:50 +0200

    Update Hello World

--- server/src/hello-world/hello-world.module.ts
+++ server/src/hello-world/hello-world.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HelloWorldController } from './hello-world.controller';
 import { HelloWorldService } from './hello-world.service';
+import { HelloWorld } from './hello-world.model';
 
 @Module({
+    imports: [HelloWorld],
     controllers: [HelloWorldController],
     providers: [HelloWorldService]
 })
