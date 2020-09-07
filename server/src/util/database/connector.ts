15e7a975fb504ed6d9a1dcb3da64ff141b1fec67
Grimmig18 <benno.grimm@gmx.de>
2020-09-07 14:02:31 +0200

    Change indents

--- server/src/util/database/connector.ts
+++ server/src/util/database/connector.ts
@@ -1,37 +1,39 @@
- import { InternalServerErrorException } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common';
 import { Query } from './query.model';
  
- export class Connector {
-  private static mariadb = require('mariadb');
-  private static credentials = require('../../../database.json');
-  private static pool: any = Connector.mariadb.createPool(
-    {
+export class Connector {
+	private static mariadb = require('mariadb');
+	private static credentials = require('../../../database.json');
+	private static pool: any = Connector.mariadb.createPool({
 		host: Connector.credentials.host,
 		port: Connector.credentials.port,
 		user: Connector.credentials.user,
 		password: Connector.credentials.password,
 		database: Connector.credentials.database,
 		connectionLimit: Connector.credentials.connectionLimit
-    });
+	});
 
-    // TODO: Create functions
-    
-    public static async executeQuery(
-      q: Query): Promise<any> {
-      let result = null;
-      let connection = null;
-      try {
-        connection = await Connector.pool.getConnection();
-        result = await connection.query(q.query, q.args);
-      } catch(err) {
-        console.log(err);
-        throw new InternalServerErrorException("Something went wrong");
-      } finally {
-        // Close connection
-        connection.release();
-      }
-      
-      return result;
-    }
+	// TODO: Create functions
+	
+	/**
+	 * 
+	 * @param q Parameter of type Query: <pre><code>{query: string, args: any[]}</code></pre>
+	 */
+	public static async executeQuery(q: Query): Promise<any> {
+		let result = null;
+		let connection = null;
+		try {
+			connection = await Connector.pool.getConnection();
+			result = await connection.query(q.query, q.args);
+	  	} catch(err) {
+			console.log(err);
+			throw new InternalServerErrorException("Something went wrong");
+	  	} finally {
+			// Close connection
+			connection.release();
+		}
+
+	  	return result;
+	}
 
 }
