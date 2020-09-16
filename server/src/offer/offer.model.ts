a9aa8addf772663b03bf5a2e9a6a1f178a2c2711
NerdyStuff <NerdyStuff@users.noreply.github.com>
2020-09-16 22:21:56 +0200

    Update getAllOffers; getOfferById

--- server/src/offer/offer.model.ts
+++ server/src/offer/offer.model.ts
@@ -6,20 +6,25 @@ export interface Offer {
     description: string,
     number_of_ratings: number,
     rating: number,
-    category_id: number,
-    user_id: string,
     price: number,
+    category: {
+        name: string,
+        category_id: number,
+        picture_link?: string
+    },
     picture_links?: Array<string>,
     blocked_dates?: Array<{
         from_date: Date,
         to_date: Date
     }>,
-    user?: {
+    lessor?: {
         first_name: string,
         last_name: string,
+        user_id: string,
         post_code: string,
         city: string,
         verified: boolean,
-        rating: number
+        lessor_rating: number
+        number_of_lessor_ratings: number
     }
 }
