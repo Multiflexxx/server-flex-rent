bcd5cd25c746ea31899915f355d1565771c1e4c1
Grimmig18 <benno.grimm@gmx.de>
2020-12-05 15:18:28 +0100

    Generate Files for chat

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { OfferModule } from './offer/offer.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [UserModule, OfferModule]
+  imports: [UserModule, OfferModule, ChatModule],
 })
 export class AppModule {}
