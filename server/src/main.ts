91d7ee5bbc234583b455baaa5fc71f036e4a17e7
Grimmig18 <benno.grimm@gmx.de>
2021-01-23 14:07:56 +0100

    Bugfix: Remove import causing problems

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import { CronJobs } from './util/cron/cron-jobs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
