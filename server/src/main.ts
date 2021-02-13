1bade44af19f96eca4268cea844807199ad2a3d6
NerdyStuff <NerdyStuff@users.noreply.github.com>
2021-02-13 20:13:04 +0100

    Add line to import cronJobs file

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { CronJobs } from './util/cron/cron-jobs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
