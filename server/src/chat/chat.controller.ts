572983fda107cfb35faba648eb5c5b6973decce6
NerdyStuff <NerdyStuff@users.noreply.github.com>
2021-02-25 18:00:24 +0100

    FLX-15 Add endpoints for chat

--- server/src/chat/chat.controller.ts
+++ server/src/chat/chat.controller.ts
@@ -1,4 +1,50 @@
 import { Controller } from '@nestjs/common';
+import { Get, Param, Put, Patch, Delete, Query, Body, Post, UseInterceptors, UploadedFiles, Res, NotFoundException, NotImplementedException } from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import * as StaticConsts from 'src/util/static-consts';
+import { ChatMessage } from './chat-message.model';
+const fileConfig = require('../../file-handler-config.json');
 
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+    constructor(private readonly chatService: ChatService) { }
+
+    /**
+     * Accepts up to ten files to upload images
+     * @param images field key for files array
+     */
+    @Post('images')
+    @UseInterceptors(FilesInterceptor('images', StaticConsts.MAX_NUMBER_OF_OFFER_IMAGES))
+    uploadChatPicture(
+        @UploadedFiles() images,
+        @Body() reqBody: {}
+    ) {
+        return this.chatService.uploadPictures(reqBody, images);
+    }
+
+    /**
+     * Is used to create a new chat message, returns the sent message
+     * @param reqBody message body
+     */
+    @Put()
+    receiveChatMessage(
+        @Body() reqBody: {}
+    ) {
+        return this.chatService.receiveChatMessage(reqBody);
+    }
+
+    /**
+     * Returns the that messages for a given chat id after authemntication
+     * @param id Chat id
+     * @param reqBody authentication
+     */
+    @Post(':id')
+    getMessagesByChatId(
+        @Param('id') id: string,
+        @Body() reqBody: {}
+    ) {
+        return this.chatService.getMessagesByChatId(id, reqBody);
+    }
+
+}
--- server/src/chat/chat.service.ts
+++ server/src/chat/chat.service.ts
@@ -1,6 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotImplementedException } from '@nestjs/common';
+import { UserSession } from 'src/user/user-session.model';
+import { ChatMessage } from './chat-message.model';
 
 @Injectable()
 export class ChatService {
-    
+
+    /**
+     * Used to upload chat pictures
+     * @param reqBody authentication of users and chatmessage
+     * @param images image files
+     */
+    public async uploadPictures(reqBody: {
+        session?: UserSession,
+        message?: ChatMessage
+    },
+    images: any): Promise<ChatMessage> {
+        throw new NotImplementedException("Not implemented yet!");
+    }
+
+    /**
+     * used to get Messages from Client
+     * @param reqBody Authentication of user and chat message
+     */
+    public async receiveChatMessage(reqBody: {
+        session?: UserSession,
+        message?: ChatMessage
+    }): Promise<ChatMessage> {
+        throw new NotImplementedException("Not implemented yet!");
+    }
+
+    /**
+     * Used to get chat messages by chat id
+     * @param id id of chat
+     * @param reqBody authentication of user
+     */
+    public async getMessagesByChatId(id: string, reqBody: {
+        session?: UserSession
+    }): Promise<{
+        messages: Array<ChatMessage>,
+		current_page: number,
+		max_page: number,
+		messages_per_page: number
+    }> {
+        throw new NotImplementedException("Not implemented yet!");
+    }
 }
