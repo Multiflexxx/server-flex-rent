66ab3e97e6797217c9fa6413eb1fc10b724f96bb
NerdyStuff <NerdyStuff@users.noreply.github.com>
2021-02-25 16:58:27 +0100

    FLX-15 Add message object type

---
+++ server/src/chat/chat-message.model.ts
@@ -0,0 +1,9 @@
+export interface ChatMessage {
+    chat_id: string,
+    from_user_id: string,
+    to_user_id: string,
+    message_content: string,
+    message_type: string,
+    status_id: number,
+    created_at: Date
+}
