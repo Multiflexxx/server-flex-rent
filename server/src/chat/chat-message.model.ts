41c221f41f112800f9ce3e0035a93edc266f4bdf
NerdyStuff <NerdyStuff@users.noreply.github.com>
2021-02-27 21:51:53 +0100

    Fixed wrong type in chatmessage object

--- server/src/chat/chat-message.model.ts
+++ server/src/chat/chat-message.model.ts
@@ -3,7 +3,7 @@ export interface ChatMessage {
     from_user_id: string,
     to_user_id: string,
     message_content: string,
-    message_type: string,
+    message_type: number,
     status_id: number,
     created_at: Date,
     message_id?: string
