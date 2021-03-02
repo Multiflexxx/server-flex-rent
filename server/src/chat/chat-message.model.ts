e54acc97a834ba50f50a78f0e3ef375c5e031fb4
Benno Grimm <43752927+Grimmig18@users.noreply.github.com>
2021-03-02 17:30:54 +0100

    FLX-15 fix for new paging

--- server/src/chat/chat-message.model.ts
+++ server/src/chat/chat-message.model.ts
@@ -1,11 +1,11 @@
 export interface ChatMessage {
     chat_id: string,
-    message_count: number,
     from_user_id: string,
     to_user_id: string,
     message_content: string,
     message_type: number,
     status_id: number,
     created_at?: Date,
-    message_id?: string
+    message_id?: string,
+    message_count?: number,
 }
